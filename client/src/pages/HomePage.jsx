04a6e73c9aafcbbfa32d93a700d07548cb9e128a
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-20 20:50:24 +0545

    Adding components for rateLimited

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
+import RateLimitedUI from '../components/RateLimitedUI';
 
 const HomePage = () => {
+  const [isRateLimited, setRateLimited] = useState(false);
   return (
     <div className='min-h-screen'>
       <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
     </div>
   );
 };
