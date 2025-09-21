3fb2688ea7134ba96667486871bab4d520379219
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-21 21:32:34 +0545

    Implementing rate limiter

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 import Navbar from '../components/Navbar';
 import RateLimitedUI from '../components/RateLimitedUI';
 
 const HomePage = () => {
   const [isRateLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/api/notes');
+        console.log(res.data);
+        setNotes(res.data);
+        setRateLimited(false);
+      } catch (error) {
+        console.log('Error fetching notes');
+        if (error.response.status === 429) {
+          setRateLimited(true);
+        } else {
+          toast.error('Error!!! Failed to load notes');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
   return (
     <div className='min-h-screen'>
       <Navbar />
