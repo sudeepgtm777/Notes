e24857b9632a86c38dc77691fcd522aa4166130e
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-22 17:43:33 +0545

    Creating card and rendering

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import Navbar from '../components/Navbar';
 import RateLimitedUI from '../components/RateLimitedUI';
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
   const [isRateLimited, setRateLimited] = useState(false);
@@ -14,7 +15,7 @@ const HomePage = () => {
       try {
         const res = await axios.get('http://localhost:3000/api/notes');
         console.log(res.data);
-        setNotes(res.data);
+        setNotes(res.data.data.notes);
         setRateLimited(false);
       } catch (error) {
         console.log('Error fetching notes');
@@ -34,6 +35,19 @@ const HomePage = () => {
       <Navbar />
 
       {isRateLimited && <RateLimitedUI />}
+
+      <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {loading && (
+          <div className='text-center text-primary py-10'>Loading Notes...</div>
+        )}
+        {notes.length > 0 && !isRateLimited && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map((note) => {
+              return <NoteCard key={note._id} note={note} />;
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
