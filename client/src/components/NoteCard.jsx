c213b419298f31b95799e2c578ad5c0088bbeec4
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-22 17:49:08 +0545

    Debugging color

--- client/src/components/NoteCard.jsx
+++ client/src/components/NoteCard.jsx
@@ -6,7 +6,7 @@ const NoteCard = ({ note }) => {
   return (
     <Link
       to={`/notes/${note._id}`}
-      className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#8F00FF'
+      className='card bg-base-200 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#fffff]'
     >
       <div className='card-body'>
         <h3 className='card-title text-base-content'>{note.title}</h3>
