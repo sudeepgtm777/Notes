8707c7b44767831c31efcb2776a423ae5d0bbb4d
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-22 19:40:28 +0545

    Creating Create new note page

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ArrowLeftIcon } from 'lucide-react';
+import { Link } from 'react-router';
 
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className='min-h-screen bg-base-200'>
+      <div className='max-w-2xl mx-auto'>
+        <Link to={'/'} className='btn btn-ghost mb-6'>
+          <ArrowLeftIcon className='size-5' />
+          Back to Notes
+        </Link>
+
+        <div className='card bg-base-100'>
+          <div className='card-body'>
+            <h2 className='card-title text-2xl mb-4'>Create New Notes</h2>
+            <form onSubmit={handleSubmit}>
+              <div className='form-control mb-4'>
+                <label className='label'>
+                  <span className='label-text'>Title</span>
+                </label>
+                <input
+                  type='text'
+                  placeholder='Note Title'
+                  className='input input-bordered'
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className='form-control mb-4'>
+                <label className='label'>
+                  <span className='label-text'>Content</span>
+                </label>
+                <textarea
+                  placeholder='Write Your Note...'
+                  className='textarea textarea-bordered h-32'
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                />
+              </div>
+              <div className='card-actions justify-end'>
+                <button
+                  type='submit'
+                  className='btn btn-primary'
+                  disabled={loading}
+                >
+                  {loading ? 'Creating...' : 'Create Note'}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
